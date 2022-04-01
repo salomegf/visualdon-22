97145d4f942456fc0b4dcdb57333635794fd317b
Salomé Gfeller <salome.gfeller@heig-vd.ch>
2022-04-01 16:25:40 +0200

    ex4 b

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,4 +1,4 @@
-import * as d3 from 'd3'
+//import * as d3 from 'd3'
 import {
     scaleSqrt
 } from 'd3'
@@ -41,6 +41,8 @@ countries.forEach(country => {
     i++;
 });
 
+console.log(y2021);
+
 //Marges
 const margin = {
         top: 20,
@@ -51,7 +53,7 @@ const margin = {
     width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-const svg = d3.select("svg");
+const body = d3.select("body");
 
 //Échelles
 const x = d3.scaleSqrt()
@@ -63,7 +65,8 @@ const y = d3.scaleLinear()
     .range([height, 0])
 
 //SVG + translation
-svg.attr("width", width + margin.left + margin.right)
+body.append('svg')
+    .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g").attr("class", "group")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
@@ -94,3 +97,100 @@ g.selectAll("circle")
     .style("fill", "orange")
     .style("stroke", "orange")
     .style("fill-opacity", 0.3);
+
+
+
+// Cartographie
+const margin2 = {
+        top: 5,
+        right: 5,
+        bottom: 5,
+        left: 5
+    },
+    width2 = 600 - margin2.left - margin2.right,
+    height2 = 400 - margin2.top - margin2.bottom;
+
+body.append('svg')
+    .attr("width", width2 + margin2.left + margin2.right)
+    .attr("height", height2 + margin2.top + margin2.bottom)
+    .append("g").attr("class", "group2")
+    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")")
+
+const g2 = d3.select(".group2");
+
+// Map and projection
+const path = d3.geoPath();
+const projection = d3.geoMercator()
+    .scale(70)
+    .center([0, 20])
+    .translate([width / 2, height / 2]);
+
+// Data and color scale
+//const data = d3.map();
+const colorScale = d3.scaleThreshold()
+    .domain([50, 60, 70, 80, 90])
+    .range(d3.schemeOranges[5]);
+
+// Load external data and boot
+d3.queue()
+    .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+    /* .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function (d) {
+        data.set(d.code, +d.pop);
+    }) */
+    .await(ready);
+
+function ready(error, topo) {
+
+    let mouseOver = function (d) {
+        d3.selectAll(".Country")
+            .transition()
+            .duration(200)
+            .style("opacity", .5)
+        d3.select(this)
+            .transition()
+            .duration(200)
+            .style("opacity", 1)
+            .style("stroke", "black")
+    }
+
+    let mouseLeave = function (d) {
+        d3.selectAll(".Country")
+            .transition()
+            .duration(200)
+            .style("opacity", .8)
+        d3.select(this)
+            .transition()
+            .duration(200)
+            .style("stroke", "transparent")
+    }
+
+    // Draw the map
+    g2.append("g")
+        .selectAll("path")
+        .data(topo.features)
+        .enter()
+        .append("path")
+        // draw each country
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        // set the color of each country
+        .attr("fill", function (d, i) {
+            const country = y2021.filter(y2021 => y2021.country == d.properties.name)
+            
+            let lifeExp = ''
+            if (country[0] === undefined) {
+                lifeExp = 0;
+            } else {
+                lifeExp = country[0].life_expectancy
+            }
+            return colorScale(lifeExp);
+        })
+        .style("stroke", "transparent")
+        .attr("class", function (d) {
+            return d.properties.name
+        })
+        .style("opacity", .8)
+        .on("mouseover", mouseOver)
+        .on("mouseleave", mouseLeave)
+}
