26fcb9da46455b62a1fa89a4a77999eddf7f54ce
Salomé Gfeller <salome.gfeller@heig-vd.ch>
2022-04-24 15:28:34 +0200

    ex5 start

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-import * as d3 from 'd3';
-import puppeteer from 'puppeteer';
+import * as puppeteer from "puppeteer";
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -8,10 +6,10 @@ import puppeteer from 'puppeteer';
 
     //await page.setViewport({ width: 1280, height: 800 })
     await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
-    //await page.screenshot({ path: 'cantons.png' });
+    await page.screenshot({ path: 'cantons.png' });
 
     await browser.close();
 })();
-=======
-import * as puppeteer from "puppeteer";
->>>>>>> d94735becdc88ec3a995e077f2c8b40b2c77c556
+
+screenshot();
+console.log('yoo');
